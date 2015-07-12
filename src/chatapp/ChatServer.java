2fc1c16b8573385be21693678ffb956e8b3f33c3
Altraum <great.ore@gmail.com>
2015-07-12 12:07:40 -0400

    Haven't tested to be sure it works on other computers

---
+++ src/chatapp/ChatServer.java
@@ -0,0 +1,100 @@
+/********************************************
+*	Basil Grant                             *
+*	12/2/14                                 *
+*	Mini-Project							*
+********************************************/
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.sql.SQLException;
+
+public class ChatServer extends JFrame implements Runnable {
+    
+    public void run() {}
+    
+    public ChatServer() {
+        this.setLayout(new BorderLayout());
+	JPanel old = new JPanel();
+	old.setLayout(new BorderLayout());
+        this.setTitle("Server Messenger");
+	this.add(old);
+        this.setSize(500,300);
+	this.setVisible(true);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        JPanel top = new JPanel(new FlowLayout());
+	JPanel center = new JPanel(new BorderLayout());
+	JPanel bottom = new JPanel(new FlowLayout());
+        TextField userMessage= new TextField();
+        TextArea convoArea = new TextArea();
+        convoArea.setEditable(false);
+        JButton sendButton = new JButton("Send");
+        old.add(top, BorderLayout.PAGE_START);
+	old.add(center, BorderLayout.CENTER);
+	old.add(bottom, BorderLayout.PAGE_END);
+        top.add(convoArea);
+        center.add(userMessage, BorderLayout.CENTER);
+        bottom.add(sendButton);
+        
+        try{
+            ServerSocket serverSocket = new ServerSocket(8000);
+            
+            Socket socket = serverSocket.accept();
+            
+            DataInputStream inputFromClient = new DataInputStream(socket.getInputStream());
+            DataOutputStream outputToClient = new DataOutputStream(socket.getOutputStream());
+            Thread receiveThread = new Thread(){
+                public void run () {
+                    while(true){
+                    String receive;
+                    try{
+                        receive = inputFromClient.readUTF();
+                        convoArea.append("Client: " + receive + "\n");
+                    }
+                    
+                    catch (IOException ex){
+                        System.err.println(ex);
+                    }
+                    }
+                }
+            };
+            receiveThread.start();
+            Thread sendThread = new Thread() {
+                    
+                public void run () {
+                sendButton.addActionListener(  
+                    new ActionListener()  {
+                        public void actionPerformed(ActionEvent e) {
+                            try{
+                                String send;
+                                send = userMessage.getText();
+                                convoArea.append("Server: " + send + "\n");
+                                outputToClient.writeUTF(send);
+                            }
+                            catch (IOException ex) {
+                                System.err.println(ex);
+                            }
+                            userMessage.setText(null);
+                        }
+                    }
+                );
+                }
+            };
+            sendThread.start();
+            
+            
+        }
+        catch(IOException ex){
+            System.err.println(ex);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer();
+    }
+    
+}
